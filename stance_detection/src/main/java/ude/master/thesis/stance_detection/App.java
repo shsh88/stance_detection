87fe11344951e6185ef7f5d0da838785d56e015a
Razan <Razan@razan-pc>
2017-07-24 15:34:18 +0200

    first commit

---
+++ stance_detection/src/main/java/ude/master/thesis/stance_detection/App.java
@@ -0,0 +1,13 @@
+package ude.master.thesis.stance_detection;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
