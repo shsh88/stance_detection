e05c9c634636c7eaa9a03d50b14c73682088e249
Razan <Razan@132.252.237.247>
2017-08-14 11:48:43 +0200

    testing code lines

--- stance_detection/src/main/java/ude/master/thesis/stance_detection/main/StanceClassifier.java
+++ stance_detection/src/main/java/ude/master/thesis/stance_detection/main/StanceClassifier.java
@@ -10,6 +10,8 @@ import ude.master.thesis.stance_detection.ml.MainClassifier;
 import ude.master.thesis.stance_detection.util.StanceDetectionDataReader;
 import weka.classifiers.functions.SMO;
 import weka.classifiers.functions.LibSVM;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
 
 /**
  * 
@@ -21,7 +23,7 @@ public class StanceClassifier {
 	public static final boolean USE_TRAINING_SET = true;
 	public static final boolean USE_TEST_SET = true;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 
 		long start = System.currentTimeMillis();
 
@@ -38,6 +40,16 @@ public class StanceClassifier {
 		MainClassifier classifier = new MainClassifier(trainingIdBodyMap, trainingStances, testIdBodyMap, testStances,
 				new LibSVM());
 
+		DataSource trainDataSource = new DataSource("resources/arff_data/baseline_features08-01_02-19.arff");
+		Instances trainData = trainDataSource.getDataSet();
+		trainData.setClassIndex(trainData.numAttributes() - 1);
+		classifier.setTrainingInstances(trainData);
+
+		DataSource testDataSource = new DataSource("resources/arff_data/baseline_features08-01_02-19_test.arff");
+		Instances testData = testDataSource.getDataSet();
+		testData.setClassIndex(testData.numAttributes() - 1);
+		classifier.setTestInstances(testData);
+
 		classifier.setUseOverlapFeature(true);
 		classifier.setUseRefutingFeatures(true);
 		classifier.setUsePolarityFeatures(true);
@@ -45,13 +57,12 @@ public class StanceClassifier {
 		classifier.setUseBinaryCooccurraneStopFeatures(true);
 		classifier.setUseCharGramsFeatures(true);
 		classifier.setUseWordGramsFeatures(true);
-		//classifier.setUseTitle(true);
-		//classifier.setUseArticle(true);
-		//classifier.setUseWordGramsFeatures(true);
+		// classifier.setUseTitle(true);
+		// classifier.setUseArticle(true);
 		classifier.evaluate();
-		classifier.saveInstancesToArff("baseline_features_BoW" + getCurrentTimeStamp());
+		//classifier.saveInstancesToArff("baseline_features_" + getCurrentTimeStamp());
 
-		//classifier.train();
+		// classifier.train();
 
 		System.out.println(System.currentTimeMillis() - start);
 	}
