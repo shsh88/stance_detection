de8f870dce0266ea4e4d80df4def28f5fdd680ff
Razan <Razan@razan-pc.SpeedportEntry2090126200030>
2017-09-01 12:04:58 +0200

    saving dependencies hash files

--- stance_detection/src/main/java/ude/master/thesis/stance_detection/processor/StanfordDependencyParser.java
+++ stance_detection/src/main/java/ude/master/thesis/stance_detection/processor/StanfordDependencyParser.java
@@ -5,38 +5,39 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
+
+import org.clapper.util.misc.FileHashMap;
+import org.clapper.util.misc.ObjectExistsException;
+import org.clapper.util.misc.VersionMismatchException;
 
 import com.opencsv.CSVWriter;
 
-import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.semgraph.SemanticGraph;
-import edu.stanford.nlp.semgraph.SemanticGraphEdge;
-import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.EnhancedDependenciesAnnotation;
 import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.EnhancedPlusPlusDependenciesAnnotation;
-import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.BasicDependenciesAnnotation;
-import edu.stanford.nlp.trees.GrammaticalRelation;
 import edu.stanford.nlp.util.CoreMap;
 import ude.master.thesis.stance_detection.util.StanceDetectionDataReader;
 
 public class StanfordDependencyParser {
 
+	private static boolean pipeInitialized = false;
+	private static StanfordCoreNLP pipeline;
+
 	public static Annotation buildAnnotatedDoc(String text) {
 
-		// creates a StanfordCoreNLP object, with POS tagging, lemmatization,
-		// NER, parsing, and coreference resolution
-		Properties props = new Properties();
-		props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
-		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+		if (!pipeInitialized)
+			initPipe();
 
 		// create an empty Annotation just with the given text
 		Annotation document = new Annotation(text);
@@ -46,6 +47,15 @@ public class StanfordDependencyParser {
 
 		return document;
 
+	}
+
+	private static void initPipe() {
+		// creates a StanfordCoreNLP object, with POS tagging, lemmatization,
+		// NER, parsing, and coreference resolution
+		Properties props = new Properties();
+		props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+		pipeline = new StanfordCoreNLP(props);
+		pipeInitialized = true;
 	}
 
 	public static List<SemanticGraph> buildDependencyGraph(Annotation document) {
@@ -76,15 +86,15 @@ public class StanfordDependencyParser {
 		return lemmas;
 	}
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ObjectExistsException, ClassNotFoundException, VersionMismatchException {
 		StanceDetectionDataReader sddr = new StanceDetectionDataReader(true, true,
 				"resources/data/summ_train_bodies.csv", "resources/data/test_data/summ_competition_test_bodies.csv");
 
 		// Map<Integer, String> trainIdBodyMap = sddr.getTrainIdBodyMap();
 		// generaateRootDistFeaturesAndSave(trainIdBodyMap,
 		// "C:/thesis_stuff/features/train_rootdist.csv");
-		HashMap<Integer, String> testIdBodyMap = sddr.getTestIdBodyMap();
-		generaateRootDistFeaturesAndSave(testIdBodyMap, "C:/thesis_stuff/features/test_rootdist.csv");
+		//HashMap<Integer, String> testIdBodyMap = sddr.getTestIdBodyMap();
+		//generaateRootDistFeaturesAndSave(testIdBodyMap, "C:/thesis_stuff/features/test_rootdist.csv");
 
 		/*
 		 * String text3 =
@@ -93,6 +103,18 @@ public class StanfordDependencyParser {
 		 * graphs = buildDependencyGraph(doc); List<Integer> rootDistVec =
 		 * getRooDistFeatureVec(graphs); System.out.println(rootDistVec);
 		 */
+		
+		//Task: save dependencies already
+		//saveDepsListsMaps();
+		
+		String txt = "Kim Jong-un has broken both of his ankles and is now in the hospital after undergoing "
+				+ "surgery, a report in a South Korean newspaper claims. The North Korean leader has "
+				+ "been missing for more than three weeks, fueling speculation about what could cause his "
+				+ "unusual disappearance from the public eye. This rumor seems to confirm what North Korean "
+				+ "state media had said on Thursday, when state broadcaster Korean Central Television "
+				+ "reported that Kim was \"not feeling well,\" and was suffering from an \"uncomfortable "
+				+ "physical condition.\" Have something to add to this story? Share it in the comments.";
+		System.out.println(getDependenciesAsTxtList(txt));
 	}
 
 	private static void generaateRootDistFeaturesAndSave(Map<Integer, String> trainIdBodyMap, String filename)
@@ -142,12 +164,14 @@ public class StanfordDependencyParser {
 		int id = 0;
 
 		for (SemanticGraph graph : graphs) {
-			IndexedWord root = graph.getFirstRoot(); // TODO: it it precise to take only the first root
+			IndexedWord root = graph.getFirstRoot(); // TODO: it it precise to
+														// take only the first
+														// root
 			for (int i = 1; i <= graph.size(); i++) {
 				// System.out.println("graph size: " + graph.size());
 				IndexedWord idxW = graph.getNodeByIndexSafe(i);
 				System.out.println("is idxW null " + idxW == null);
-				//System.out.println(idxW.word());
+				// System.out.println(idxW.word());
 
 				if ((idxW != null))
 					if (idxW.word() != null)
@@ -195,6 +219,93 @@ public class StanfordDependencyParser {
 			mins_avgs.add(avgd / id);
 
 		return mins_avgs;
+	}
+
+	public static void saveDepsListsMaps()
+			throws IOException, ObjectExistsException, ClassNotFoundException, VersionMismatchException {
+		StanceDetectionDataReader sddr = new StanceDetectionDataReader(true, true, "resources/data/train_stances.csv",
+				"resources/data/summ_train_bodies.csv", "resources/data/test_data/competition_test_stances.csv",
+				"resources/data/test_data/summ_competition_test_bodies.csv");
+
+		// ******************* Do training data ****************************
+		Map<Integer, String> trainIdBodyMap = sddr.getTrainIdBodyMap();
+		List<List<String>> trainingStances = sddr.getTrainStances();
+
+		// Do the training bodies
+		FileHashMap<Integer, List<String>> trainBDepsMap = new FileHashMap<Integer, List<String>>(
+				"C:/thesis_stuff/help_files/train_bodies_deps", FileHashMap.FORCE_OVERWRITE);
+		for (Map.Entry<Integer, String> b : trainIdBodyMap.entrySet()) {
+			List<String> deps = getDependenciesAsTxtList(b.getValue());
+			trainBDepsMap.put(b.getKey(), deps);
+		}
+		trainBDepsMap.save();
+		trainBDepsMap.close();
+
+		// Do the titles
+		FileHashMap<String, List<String>> trainTDepsMap = new FileHashMap<String, List<String>>(
+				"C:/thesis_stuff/help_files/train_titles_deps", FileHashMap.FORCE_OVERWRITE);
+		Set<String> testTitlesSet = new HashSet<>();
+		for (List<String> s : trainingStances) {
+			testTitlesSet.add(s.get(0));
+		}
+		for (String t : testTitlesSet) {
+			List<String> deps = getDependenciesAsTxtList(t);
+			trainTDepsMap.put(t, deps);
+		}
+		trainTDepsMap.save();
+		trainTDepsMap.close();
+
+		// ******************* Do Test data ****************************
+		HashMap<Integer, String> testIdBodyMap = sddr.getTestIdBodyMap();
+		List<List<String>> testStances = sddr.getTestStances();
+
+		// Do the training bodies
+		FileHashMap<Integer, List<String>> testBDepsMap = new FileHashMap<Integer, List<String>>(
+				"C:/thesis_stuff/help_files/test_bodies_deps", FileHashMap.FORCE_OVERWRITE);
+		for (Map.Entry<Integer, String> b : testIdBodyMap.entrySet()) {
+			List<String> deps = getDependenciesAsTxtList(b.getValue());
+			testBDepsMap.put(b.getKey(), deps);
+		}
+		testBDepsMap.save();
+		testBDepsMap.close();
+
+		// Do the titles
+		FileHashMap<String, List<String>> testTDepsMap = new FileHashMap<String, List<String>>(
+				"C:/thesis_stuff/help_files/test_titles_deps", FileHashMap.FORCE_OVERWRITE);
+		Set<String> trainTitlesSet = new HashSet<>();
+		for (List<String> s : testStances) {
+			trainTitlesSet.add(s.get(0));
+		}
+		for (String t : trainTitlesSet) {
+			List<String> deps = getDependenciesAsTxtList(t);
+			testTDepsMap.put(t, deps);
+		}
+		testTDepsMap.save();
+		testTDepsMap.close();
+	}
+
+	private static List<String> getDependenciesAsTxtList(String text) {
+		Annotation doc = StanfordDependencyParser.buildAnnotatedDoc(text);
+
+		List<SemanticGraph> graphs = StanfordDependencyParser.buildDependencyGraph(doc);
+
+		List<String> depLists = new ArrayList<>();
+
+		List<Integer> negIdxs = new ArrayList<>();
+		int i = 0; // Sentence / Graph Index
+		for (SemanticGraph graph : graphs) {
+
+			String depList = graph.toList();
+			
+			int size = graph.size();
+			/*for(int j = 1; j < size; j++){
+				String l = graph.getNodeByIndexSafe(j).lemma();
+				System.out.println(l);
+			}*/
+			System.out.println(graph.toRecoveredSentenceString());
+			depLists.add(depList);
+		}
+		return depLists;
 	}
 
 }
