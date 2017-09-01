3c1c4bdb797f53b658928fca86161c9e5441915e
Razan <Razan@razan-pc.SpeedportEntry2090126200030>
2017-09-01 12:08:17 +0200

    complete implementation of SvO features

--- stance_detection/src/main/java/ude/master/thesis/stance_detection/processor/SVOFeaturesGenerator.java
+++ stance_detection/src/main/java/ude/master/thesis/stance_detection/processor/SVOFeaturesGenerator.java
@@ -1,158 +1,341 @@
 package ude.master.thesis.stance_detection.processor;
 
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
+import javax.annotation.CheckForNull;
+import javax.annotation.Nullable;
+
+import org.clapper.util.misc.FileHashMap;
+import org.clapper.util.misc.ObjectExistsException;
+import org.clapper.util.misc.VersionMismatchException;
+
+import com.opencsv.CSVWriter;
+
 import edu.stanford.nlp.ie.util.RelationTriple;
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
 import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.semgraph.SemanticGraph;
-import edu.stanford.nlp.semgraph.semgrex.SemgrexMatcher;
-import edu.stanford.nlp.semgraph.semgrex.SemgrexPattern;
 import edu.stanford.nlp.util.CoreMap;
-import edu.stanford.nlp.simple.Sentence;
 import edu.stanford.nlp.trees.GrammaticalRelation;
 import edu.stanford.nlp.naturalli.NaturalLogicAnnotations;
+import ude.master.thesis.stance_detection.util.PPDBProcessor;
 import ude.master.thesis.stance_detection.util.StanceDetectionDataReader;
 
 public class SVOFeaturesGenerator {
 
-	public static void main(String[] args) throws IOException {
-		StanceDetectionDataReader sddr = new StanceDetectionDataReader(true, true, "resources/data/train_stances.csv",
-				"resources/data/summ_train_bodies.csv", "resources/data/test_data/competition_test_stances.csv",
-				"resources/data/test_data/summ_competition_test_bodies.csv");
+	// private static BufferedWriter out;
 
-		Map<Integer, String> trainIdBodyMap = sddr.getTrainIdBodyMap();
-		List<List<String>> trainingStances = sddr.getTrainStances();
-		HashMap<Integer, String> testIdBodyMap = sddr.getTestIdBodyMap();
-		List<List<String>> testStances = sddr.getTestStances();
+	private static Map<String, Integer> entailmentsMap;
+	private static Map<Integer, String> trainIdBodyMap;
+	private static List<List<String>> trainingStances;
+	private static HashMap<Integer, String> testIdBodyMap;
+	private static List<List<String>> testStances;
+	private static FileHashMap<String, List<Map<String, String>>> titlesSVOsMap;
+	private static FileHashMap<String, List<Map<String, String>>> bodiesSVOsMap;
 
-		String headline = "ISIL Beheads American Photojournalist in Iraq";
-		String body = "James Foley, an American journalist who went missing in Syria more than a year ago, "
-				+ "has reportedly been executed by the Islamic State, a militant group formerly known as ISIS. "
-				+ "Video and photos purportedly of Foley emerged on Tuesday. A YouTube video -- entitled \"\"A "
-				+ "Message to #America (from the #IslamicState)\"\" -- identified a man on his knees as"
-				+ " \"\"James Wright Foley,\"\" and showed his execution. This is a developing story. "
-				+ "Check back here for updates.";
-
-		String headline1 = "Banksy 'arrested & real identity revealed' is the same hoax from last year";
-		String headline3 = "BrFidel Castro is dead, according to viral Twitter rumors";
-		String testTxt = "We have no information on whether users are at risk";
+	public static void main(String[] args) {
 		/*
-		 * for (List<String> s : trainingStances) {
+		 * StanceDetectionDataReader sddr = new StanceDetectionDataReader(true,
+		 * true, "resources/data/train_stances.csv",
+		 * "resources/data/summ_train_bodies.csv",
+		 * "resources/data/test_data/competition_test_stances.csv",
+		 * "resources/data/test_data/summ_competition_test_bodies.csv");
 		 * 
-		 * }
+		 * Map<Integer, String> trainIdBodyMap = sddr.getTrainIdBodyMap();
+		 * List<List<String>> trainingStances = sddr.getTrainStances();
+		 * HashMap<Integer, String> testIdBodyMap = sddr.getTestIdBodyMap();
+		 * List<List<String>> testStances = sddr.getTestStances();
+		 * 
+		 * String headline = "ISIL Beheads American Photojournalist in Iraq";
+		 * String body =
+		 * "James Foley, an American journalist who went missing in Syria more than a year ago, "
+		 * +
+		 * "has reportedly been executed by the Islamic State, a militant group formerly known as ISIS. "
+		 * +
+		 * "Video and photos purportedly of Foley emerged on Tuesday. A YouTube video -- entitled \"\"A "
+		 * +
+		 * "Message to #America (from the #IslamicState)\"\" -- identified a man on his knees as"
+		 * +
+		 * " \"\"James Wright Foley,\"\" and showed his execution. This is a developing story. "
+		 * + "Check back here for updates.";
+		 * 
+		 * String headline1 =
+		 * "Banksy 'arrested & real identity revealed' is the same hoax from last year"
+		 * ; String headline3 =
+		 * "BrFidel Castro is dead, according to viral Twitter rumors"; String
+		 * testTxt = "We have no information on whether users are at risk";
 		 */
 
-		// Create the Stanford CoreNLP pipeline
+		entailmentsMap = new HashMap<>();
+		entailmentsMap.put("ReverseEntailment", 0);
+		entailmentsMap.put("ForwardEntailment", 1);
+		entailmentsMap.put("Equivalence", 2);
+		entailmentsMap.put("OtherRelated", 2);
+		entailmentsMap.put("Independence", 3);
+
+		StanceDetectionDataReader sddr = null;
+		try {
+			sddr = new StanceDetectionDataReader(true, true, "resources/data/train_stances.csv",
+					"resources/data/summ_train_bodies.csv", "resources/data/test_data/competition_test_stances.csv",
+					"resources/data/test_data/summ_competition_test_bodies.csv");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		trainIdBodyMap = sddr.getTrainIdBodyMap();
+		trainingStances = sddr.getTrainStances();
+		testIdBodyMap = sddr.getTestIdBodyMap();
+		testStances = sddr.getTestStances();
+
+		// extractTitlesAndBodiesSVOsAndSave();
+
+		try {
+			titlesSVOsMap = new FileHashMap<String, List<Map<String, String>>>(
+					"C:/thesis_stuff/help_files/titles_svos_train_test", FileHashMap.RECLAIM_FILE_GAPS);
+			bodiesSVOsMap = new FileHashMap<String, List<Map<String, String>>>(
+					"C:/thesis_stuff/help_files/bodies_svos_train_test", FileHashMap.RECLAIM_FILE_GAPS);
+
+			generateDataSVOFeatureVector(trainingStances, "C:/thesis_stuff/features/train_svo_features.csv");
+			generateDataSVOFeatureVector(testStances, "C:/thesis_stuff/features/test_neg_features.csv");
+		} catch (ObjectExistsException | ClassNotFoundException | VersionMismatchException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+
+	}
+
+	@CheckForNull
+	public static void generateDataSVOFeatureVector(List<List<String>> stances, String csvFilepath)
+			throws FileNotFoundException, IOException {
+		List<String[]> entries = new ArrayList<>();
+		entries.add(new String[] { "title", "Body ID", "Stance", "svo_feature" });
+
+		int k = 0;
+		// get svos for training data
+		for (List<String> s : stances) {
+
+			List<String> entry = new ArrayList<>();
+			entry.add(s.get(0));
+			entry.add(s.get(1));
+			entry.add(s.get(2));
+
+			int vec[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+			List<Map<String, String>> t_svos = null;
+			if(titlesSVOsMap.containsKey(s.get(0)))
+				 t_svos = titlesSVOsMap.get(s.get(0));
+			else
+				System.out.println("not there 1");
+			List<Map<String, String>> b_svos = null;
+			
+			if(bodiesSVOsMap.containsKey(s.get(1)))
+			 b_svos = bodiesSVOsMap.get(s.get(1));
+			else
+				System.out.println("not there 2");
+
+			if (!t_svos.isEmpty() && !b_svos.isEmpty()) {
+				Map<int[], Double> vecMap = new HashMap<>();
+				for (Map<String, String> t_svo : t_svos) {
+					for (Map<String, String> b_svo : b_svos) {
+						int[] nsubjEntailment = calcEntailmentFeatureVector(t_svo.get("nsubj"), b_svo.get("nsubj"));
+						int[] verbEntailment = calcEntailmentFeatureVector(t_svo.get("verb"), b_svo.get("verb"));
+						int[] dobjEntailment = calcEntailmentFeatureVector(t_svo.get("dobj"), b_svo.get("dobj"));
+
+						int v[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+						for (int i = 0; i < 4; i++) {
+							v[i] = nsubjEntailment[i];
+						}
+
+						for (int i = 4; i < 8; i++) {
+							v[i] = verbEntailment[i - 4];
+						}
+
+						for (int i = 8; i < 12; i++) {
+							v[i] = dobjEntailment[i - 8];
+						}
+						vecMap.put(v, avg(v));
+
+					}
+				}
+				// get the vector with max values
+				int[] vv = vecMap.entrySet().stream()
+						.max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1).get().getKey();
+				vec = Arrays.copyOf(vv, vec.length);
+				entry.add(Arrays.toString(vec));
+				entries.add(entry.toArray(new String[0]));
+			}
+
+			entry.add(Arrays.toString(vec));
+			entries.add(entry.toArray(new String[0]));
+
+			if (k % 10000 == 0)
+				System.out.println("processed: " + k);
+			k++;
+
+		}
+		CSVWriter writer = new CSVWriter(new FileWriter(csvFilepath));
+		writer.writeAll(entries);
+		writer.flush();
+		writer.close();
+		System.out.println("saved saved saved");
+	}
+
+	private static Double avg(int[] v) {
+		double sum = 0.0;
+		for (int i = 0; i < v.length; i++)
+			sum += v[i];
+		return sum / v.length;
+	}
+
+	@CheckForNull
+	public static int[] calcEntailmentFeatureVector(String w1, String w2) throws FileNotFoundException, IOException {
+		int vec[] = { 0, 0, 0, 0 };
+		// ppdb: paraphrase, score, entailment
+		FileHashMap<String, ArrayList<ArrayList<String>>> ppdbData = PPDBProcessor
+				.loadPPDB2(PPDBProcessor.MAP_PPDB_2_XXL_ALL);
+
+		if (w1.equals(w2)) {
+			vec[entailmentsMap.get("Equivalence")] = 1;
+		}
+
+		ArrayList<ArrayList<String>> w1Paraphrases = ppdbData.get(w1);
+		if (w1Paraphrases != null) {
+			List<Integer> relationships = new ArrayList<>();
+			for (ArrayList<String> p : w1Paraphrases) {
+				if (p.get(0).equals(w2)) {
+					if (entailmentsMap.containsKey(p.get(2)))
+						relationships.add(entailmentsMap.get(p.get(2)));
+				}
+			}
+			if (!relationships.isEmpty()) {
+				Integer relationship = Collections.max(relationships);
+				vec[relationship] = 1;
+			}
+		}
+		return vec;
+
+	}
+
+	@CheckForNull
+	public static void extractTitlesAndBodiesSVOsAndSave()
+			throws IOException, ObjectExistsException, ClassNotFoundException, VersionMismatchException {
+
 		Properties props = new Properties();
 		props.setProperty("annotators", "tokenize,ssplit,pos,lemma,depparse,natlog,openie");
 		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 
-		// StanfordDependencyParser sdp = new StanfordDependencyParser();
-		/*
-		 * List<SemanticGraph> graphs =
-		 * StanfordDependencyParser.buildDependencyGraph(doc);
-		 * 
-		 * for (SemanticGraph graph : graphs) { System.out.println(graph); }
-		 */
-		Annotation doc = new Annotation(body);
+		// get titles SVOs
+		String titlesSVOsPath = "C:/thesis_stuff/help_files/titles_svos_train_test";
+		FileHashMap<String, List<Map<String, String>>> titlesSVOs = new FileHashMap<String, List<Map<String, String>>>(
+				titlesSVOsPath, FileHashMap.FORCE_OVERWRITE);
+
+		Set<String> titles = new HashSet<>();
+		for (List<String> s : trainingStances) {
+			titles.add(s.get(0));
+		}
+		for (List<String> s : testStances) {
+			titles.add(s.get(0));
+		}
+		for (String t : titles) {
+			List<Map<String, String>> svos = getSVOsFromText(pipeline, t);
+			titlesSVOs.put(t, svos);
+		}
+
+		titlesSVOs.save();
+		titlesSVOs.close();
+
+		// get bodies SVOs
+
+		String bodiesSVOsPath = "C:/thesis_stuff/help_files/bodies_svos_train_test";
+		FileHashMap<String, List<Map<String, String>>> bodiesSVOs = new FileHashMap<String, List<Map<String, String>>>(
+				bodiesSVOsPath, FileHashMap.FORCE_OVERWRITE);
+
+		for (Map.Entry<Integer, String> e : trainIdBodyMap.entrySet()) {
+			List<Map<String, String>> svos = getSVOsFromText(pipeline, e.getValue());
+			bodiesSVOs.put(String.valueOf(e.getKey()), svos);
+		}
+		for (Map.Entry<Integer, String> e : testIdBodyMap.entrySet()) {
+			List<Map<String, String>> svos = getSVOsFromText(pipeline, e.getValue());
+			bodiesSVOs.put(String.valueOf(e.getKey()), svos);
+		}
+
+		bodiesSVOs.save();
+		bodiesSVOs.close();
+
+	}
+
+	@CheckForNull
+	private static List<Map<String, String>> getSVOsFromText(StanfordCoreNLP pipeline, String t) {
+		Annotation doc = new Annotation(t);
 		pipeline.annotate(doc);
 		List<CoreMap> sentences = doc.get(SentencesAnnotation.class);
 
 		List<SemanticGraph> graphs = StanfordDependencyParser.buildDependencyGraph(doc);
-		// System.out.println("graphs size: " +graphs.size());
-		// System.out.println("Sentences size: " + sentences.size());
-		for (SemanticGraph graph : graphs) {
-			System.out.println(graph);
-			System.out.println(graph.toList());
-			// System.out.println(graph.relns(graph.getNodeByIndexSafe(3)));
-		}
+
+		// List of svos in t
+		List<Map<String, String>> svos = new ArrayList<>();
 
 		int i = 0;
-		List<Map<String, String>> svos = new ArrayList<>();
 		for (CoreMap sentence : sentences) {
-			System.out.println("I'm here");
-			System.out.println("sent. = " + sentence);
-			// Get the OpenIE triples for the sentence
 			Collection<RelationTriple> triples = sentence.get(NaturalLogicAnnotations.RelationTriplesAnnotation.class);
-			// Print the triples
-			System.out.println("triples= "+triples);
-			System.out.println("sentence " + i + new Sentence(sentence).text());
-			for (RelationTriple triple : triples) {
-				System.out.println(triple.confidence + "\t" + "subj: " + triple.subjectLemmaGloss() + "\t" + "rel: "
-						+ triple.relationLemmaGloss() + "\t" + "obj: " + triple.objectLemmaGloss() + "\t"
-						+ "allTokens: " + triple.allTokens());
 
+			for (RelationTriple triple : triples) {
 				List<CoreLabel> tokens = triple.allTokens();
 				SemanticGraph relatedGraph = graphs.get(i);
-	
-				Map<String,String> vec = new HashMap<String, String>();
-				for (CoreLabel t : tokens) {
+
+				Map<String, String> vec = new HashMap<String, String>();
+				for (CoreLabel tok : tokens) {
 					relatedGraph = graphs.get(i);
-					int tIndex = t.index();
-					System.out.println("tIdx= " + tIndex);
-					System.out.println("relns = " + relatedGraph.relns(relatedGraph.getNodeByIndexSafe(tIndex)));
+					int tIndex = tok.index();
 
 					Set<GrammaticalRelation> relns = relatedGraph.relns(relatedGraph.getNodeByIndexSafe(tIndex));
 					for (GrammaticalRelation rel : relns) {
 						if (rel.toString().equals("nsubj")) {
-							vec.put("nsubj", t.word());
+							vec.put("nsubj", tok.word());
 						}
 						if (rel.toString().equals("dobj")) {
-							vec.put("dobj", t.word());
+							vec.put("dobj", tok.word());
 						}
 					}
 
 				}
-				
-				if (vec.size() == 2){
-					//find the verb
+
+				if (vec.size() == 2) {
+					// find the verb
 					String depList = relatedGraph.toList();
 					String[] deps = depList.split("\n");
-					for(String d : deps){
+					for (String d : deps) {
 						String depType = d.substring(0, d.indexOf('('));
-						System.out.println("depType = " + depType);
-						if(depType.equals("nsubj")){
-							String betweenBrack = d.substring(d.indexOf('(')+1, d.indexOf(')'));
-						    String[] depWords = betweenBrack.split(",");
-						    System.out.println("iiiiih = " + depWords[1].substring(0, depWords[1].indexOf('-')).trim());
-						    System.out.println("kkkkk " + vec.get("nsubj"));
-						    if(depWords[1].substring(0, depWords[1].indexOf('-')).trim().equals(vec.get("nsubj"))){
-						    	vec.put("verb", depWords[0].substring(0, depWords[0].indexOf('-')).trim());
-						    	System.out.println("Woooof  " + depWords[0].substring(0, depWords[0].indexOf('-')).trim());
-						    }
+						if (depType.equals("nsubj")) {
+							String betweenBrack = d.substring(d.indexOf('(') + 1, d.indexOf(')'));
+							String[] depWords = betweenBrack.split(",");
+							if (depWords[1].substring(0, depWords[1].indexOf('-')).trim().equals(vec.get("nsubj"))) {
+								vec.put("verb", depWords[0].substring(0, depWords[0].indexOf('-')).trim());
+							}
 						}
 					}
 					svos.add(vec);
-					
-				}
 
+				}
 			}
 			i++;
 		}
-
-		System.out.println("svos = " + svos);
-
-		/*
-		 * SemgrexPattern pattern = SemgrexPattern.compile(
-		 * "{$}=root >/.subj(pass)?/ {}=subject >/.obj/ {}=object");
-		 * SemgrexMatcher matcher = pattern.matcher(new Sentence(
-		 * "A cat is sitting on the table").dependencyGraph()); while
-		 * (matcher.find()) { IndexedWord root = matcher.getNode("root");
-		 * IndexedWord subject = matcher.getNode("subject"); IndexedWord object
-		 * = matcher.getNode("object"); System.out.println(root.word() + "(" +
-		 * subject.word() + ", " + object.word()); }
-		 */
-
+		return svos;
 	}
 }
