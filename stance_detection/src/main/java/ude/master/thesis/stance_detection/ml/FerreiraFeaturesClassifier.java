5add574d6bd6058dda77cb6145e5782413bcd8ed
Razan <Razan@razan-pc.SpeedportEntry2090126200030>
2017-09-06 12:54:26 +0200

    AddeD SVO features vectors

--- stance_detection/src/main/java/ude/master/thesis/stance_detection/ml/FerreiraFeaturesClassifier.java
+++ stance_detection/src/main/java/ude/master/thesis/stance_detection/ml/FerreiraFeaturesClassifier.java
@@ -16,6 +16,7 @@ import org.clapper.util.misc.VersionMismatchException;
 
 import com.opencsv.CSVReader;
 
+import ude.master.thesis.stance_detection.util.PPDBProcessor;
 import ude.master.thesis.stance_detection.util.StanceDetectionDataReader;
 import weka.core.Attribute;
 import weka.core.DenseInstance;
@@ -37,6 +38,8 @@ public class FerreiraFeaturesClassifier {
 	private static List<List<String>> testStances;
 	private static FileHashMap<String, Double> trainW2VSim;
 	private static FileHashMap<String, Double> testW2VSim;
+	private static FileHashMap<String, ArrayList<Integer>> trainSVO;
+	private static FileHashMap<String, ArrayList<Integer>> testSVO;
 
 	public static void main(String[] args) throws IOException {
 
@@ -73,7 +76,15 @@ public class FerreiraFeaturesClassifier {
 		 * VersionMismatchException | IOException e) { // TODO Auto-generated
 		 * catch block e.printStackTrace(); }
 		 */
-
+		try {
+			saveSVOFeaturesAsHashFile("C:/thesis_stuff/features/train_features/train_svo_nosvo_features.csv", "C:/thesis_stuff/features/train_features/train_svo_nosvo_features");
+			saveSVOFeaturesAsHashFile("C:/thesis_stuff/features/test_features/test_svo_nosvo_features.csv", "C:/thesis_stuff/features/test_features/test_svo_nosvo_features");
+		} catch (ObjectExistsException | ClassNotFoundException | VersionMismatchException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		
 		try {
 			trainRootDist = loadRootDistFeaturesAsHashFile("C:/thesis_stuff/features/train_features/train_rootdist");
 			testRootDist = loadRootDistFeaturesAsHashFile("C:/thesis_stuff/features/test_features/test_rootdist");
@@ -88,6 +99,9 @@ public class FerreiraFeaturesClassifier {
 					"C:/thesis_stuff/features/train_features/train_w2v_mean_sim_formula_features");
 			testW2VSim = loadWord2VecFeaturesAsHashFile(
 					"C:/thesis_stuff/features/test_features/test_w2v_mean_sim_formula_features");
+			
+			trainSVO = loadSVOFeaturesAsHashFile("C:/thesis_stuff/features/train_features/train_svo_nosvo_features");
+			testSVO = loadSVOFeaturesAsHashFile("C:/thesis_stuff/features/test_features/test_svo_nosvo_features");
 		} catch (ObjectExistsException | ClassNotFoundException | VersionMismatchException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -104,10 +118,10 @@ public class FerreiraFeaturesClassifier {
 		testIdBodyMap = sddr.getTestIdBodyMap();
 		testStances = sddr.getTestStances();
 
-		saveARFF(trainingStances, trainRootDist, trainPPDB, trainNeg, trainW2VSim,
-				"C:/thesis_stuff/features/train_ferreira.arff");
-		saveARFF(testStances, testRootDist, testPPDB, testNeg, testW2VSim,
-				"C:/thesis_stuff/features/test_ferreira.arff");
+		saveARFF(trainingStances, trainRootDist, trainPPDB, trainNeg, trainW2VSim,trainSVO,
+				"C:/thesis_stuff/features/train_ferreira_with_svo_minus_one.arff");
+		saveARFF(testStances, testRootDist, testPPDB, testNeg, testW2VSim, testSVO,
+				"C:/thesis_stuff/features/test_ferreira_with_svo_minus_one.arff");
 
 	}
 
@@ -123,7 +137,7 @@ public class FerreiraFeaturesClassifier {
 	 */
 	private static void saveARFF(List<List<String>> stancesData, FileHashMap<String, ArrayList<Double>> rootDist,
 			FileHashMap<String, Double> ppdbhung, FileHashMap<String, Integer> negData,
-			FileHashMap<String, Double> simData, String filepath) throws IOException {
+			FileHashMap<String, Double> simData, FileHashMap<String, ArrayList<Integer>> svoData, String filepath) throws IOException {
 		ArrayList<Attribute> attributes = new ArrayList<>();
 
 		String stances[] = new String[] { "agree", "disagree", "discuss" };
@@ -134,6 +148,12 @@ public class FerreiraFeaturesClassifier {
 		attributes.add(new Attribute("ppdb"));
 		attributes.add(new Attribute("neg"));
 		attributes.add(new Attribute("w2v_sim_mean"));
+		
+		//add svo attributes
+		for(int i = 0; i < 12; i++){
+			attributes.add(new Attribute("svo_" + i));
+		}
+		
 
 		attributes.add(new Attribute("class", stanceValues));
 
@@ -152,6 +172,10 @@ public class FerreiraFeaturesClassifier {
 				Integer neg = negData.get(s.get(0) + s.get(1));
 
 				Double sim = simData.get(s.get(0) + s.get(1));
+				
+				ArrayList<Integer> svos = svoData.get(s.get(0) + s.get(1));
+				
+				
 
 				DenseInstance instance = new DenseInstance(attributes.size());
 				instance.setDataset(instances);
@@ -160,6 +184,11 @@ public class FerreiraFeaturesClassifier {
 				instance.setValue(instances.attribute("ppdb"), ppdb);
 				instance.setValue(instances.attribute("neg"), neg);
 				instance.setValue(instances.attribute("w2v_sim_mean"), sim);
+				
+				//add svo features vector
+				for(int i = 0; i < 12; i++){
+					instance.setValue(instances.attribute("svo_" + i), svos.get(i));
+				}
 
 				instance.setClassValue(stance);
 
@@ -332,8 +361,51 @@ public class FerreiraFeaturesClassifier {
 		return negData;
 	}
 
-	public static void saveSVOFeaturesAsHashFile(String filePath) {
-		// TODO SVO not finished
+	/**
+	 * 
+	 * @param csvfilePath
+	 * @param hashFileName
+	 * @throws IOException 
+	 * @throws VersionMismatchException 
+	 * @throws ClassNotFoundException 
+	 * @throws ObjectExistsException 
+	 * @throws FileNotFoundException 
+	 */
+	public static void saveSVOFeaturesAsHashFile(String csvfilePath, String hashFileName) throws FileNotFoundException, ObjectExistsException, ClassNotFoundException, VersionMismatchException, IOException {
+		FileHashMap<String, ArrayList<Integer>> svoData = new FileHashMap<String, ArrayList<Integer>>(hashFileName,
+				FileHashMap.FORCE_OVERWRITE);
+
+		CSVReader reader = null;
+		reader = new CSVReader(new FileReader(csvfilePath));
+		String[] line;
+		line = reader.readNext();
+
+		while ((line = reader.readNext()) != null) {
+			svoData.put(line[0] + line[1], PPDBProcessor.getIntList(line[3]));
+		}
+		reader.close();
+
+		// saving the map file
+		svoData.save();
+		svoData.close();
+	}
+	
+	/**
+	 * 
+	 * @param hashFileName
+	 * @return
+	 * @throws FileNotFoundException
+	 * @throws ObjectExistsException
+	 * @throws ClassNotFoundException
+	 * @throws VersionMismatchException
+	 * @throws IOException
+	 */
+	public static FileHashMap<String, ArrayList<Integer>> loadSVOFeaturesAsHashFile(String hashFileName)
+			throws FileNotFoundException, ObjectExistsException, ClassNotFoundException, VersionMismatchException,
+			IOException {
+		FileHashMap<String, ArrayList<Integer>> svoData = new FileHashMap<String, ArrayList<Integer>>(hashFileName,
+				FileHashMap.RECLAIM_FILE_GAPS);
+		return svoData;
 	}
 
 	/**
